0051de17f1c3840c4e885dad38dbc93911fe00a8
Krishna Halaharvi <halaharvi.krishna@gmail.com>
2019-02-20 09:26:36 -0600

    adding theme config to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { NavBar, NavBarItem } from './components/NavBar';
 export { default as Image } from './components/Image';
 export { default as Table } from './components/Table';
+export { default as Theme } from './hocs/withTheme';
